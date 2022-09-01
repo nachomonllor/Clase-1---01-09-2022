84b5b72e8354be8f520b804a7297a684273eb660
Ignacio Nicanor Monllor <IMONLLOR@trabajo.gob.ar>
2022-09-01 19:00:58 -0300

    primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { BienvenidoComponent } from './bienvenido/bienvenido.component';
+import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BienvenidoComponent,
+    LoginComponent,
+    ErrorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
